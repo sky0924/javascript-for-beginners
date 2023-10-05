96baccb712c4a2728761e55e20c0fc31343a36dc
sky0924 <sbd0924@gmail.com>
2023-10-05 21:06:22 +0900

    -#2.5 completed

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-alert('hi')
+/* 2.1 ~ 2.3 */
+let a = 5
+const b = '2'
+
+console.log(a + b)
+console.log(a * b)
+console.log(a / b)
+
+
+/* 2.4 */
+const amIFat = null
+let something
+console.log(amIFat, something)
+
+
+/* 2.5 */
+const daysOfWeek = ['wed', 'thu', 'fri', 'sat']
+
+// Get Item from Array
+console.log(daysOfWeek[2])
+
+// Add one more day to the array
+daysOfWeek.push("sun")
+console.log(daysOfWeek)
+
+const toBuy = ['potato', 'tomato', 'carrot']
+console.log(toBuy)
+
+toBuy.push('cabbage')
+console.log(toBuy)
+console.log(toBuy[999])
